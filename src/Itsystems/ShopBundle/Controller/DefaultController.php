d8e47dc7b79908efbfdab55747a41cdabff39aba
arbernal <aritimez@gmail.com>
2016-04-25 21:38:40 -0500

    Nuevos archivos del bundle principal ShopBundle

---
+++ src/Itsystems/ShopBundle/Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Itsystems\ShopBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('ItsystemsShopBundle:Default:index.html.twig');
+    }
+}
---
+++ src/Itsystems/ShopBundle/ItsystemsShopBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Itsystems\ShopBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class ItsystemsShopBundle extends Bundle
+{
+}
